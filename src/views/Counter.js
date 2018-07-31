b93b7716bfc2272db8427a28b87baad25bd83e56
Kacper Swietlaga <kacperres@gmail.com>
2018-07-31 15:42:56 +0200

    added-two-buttons

--- src/views/Counter.js
+++ src/views/Counter.js
@@ -10,8 +10,45 @@ class Counter extends React.Component {
     }
 
     componentDidMount() {
-        const lastState = JSON.parse(localStorage.getItem())
+        const lastState = JSON.parse(localStorage.getItem('cappy-react-counter'))
+
+        if(lastState===null) return 
+        this.setState(lastState)
+    }
+    componentWillUnmount() {
+        localStorage.setItem('cappy-react-counter', JSON.stringify(this.state))
+    }
+
+
+    incHandler =() => {
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+    decHandler = () => {
+        this.setState({
+            number: this.state.number - 1
+        })
+    }
+
+    render (){
+        return(
+            <div>
+                <h1>{this.state.number}</h1>
+                <button
+                    onClick={this.incHandler}
+                    >
+                    +
+                    </button>
+                    <button
+                    onClick={this.decHandler}
+                    >
+                    -
+                    </button>
+            </div>
+        )
     }
 
 
 }
+export default Counter
